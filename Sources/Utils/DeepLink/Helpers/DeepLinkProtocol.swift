c169acf1ae1aaf4954e2acacb5fc4cd97d1aeb76
alrzi <zinovievsasha@gmail.com>
2026-09-04 11:20:50 +0200

    Remove deep link utilities

--- Sources/Utils/DeepLink/Helpers/DeepLinkProtocol.swift
+++
@@ -1,5 +0,0 @@
-
-import Foundation
-
-/// Ссылка, которую может обработать приложение
-public protocol DeepLinkProtocol: RawRepresentable, Sendable { }
--- Sources/Utils/DeepLink/Items/DeepLinkError.swift
+++
@@ -1,8 +0,0 @@
-
-import Foundation
-
-/// Ошибка при обработке ссылок
-enum DeepLinkError: Error {
-    /// Некорректное «сырое» значение
-    case invalidRawValue
-}
